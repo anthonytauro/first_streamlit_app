0aaea675edabc6b7075056271b597e7c66a1dd97
Tony Tauro <anthony.tauro@gmail.com>
2023-02-19 16:05:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.text('testing')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
