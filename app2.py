45433919ea9246d2098f2fc52f6116c2bb481397
Tony Tauro <anthony.tauro@gmail.com>
2023-02-20 04:30:03 -0500

    Update app2.py
    Set index

--- app2.py
+++ app2.py
@@ -1,8 +1,6 @@
 import streamlit
 import pandas
 
-
-
 streamlit.title('My Parent\'s Healthy New Diner')
 
 streamlit.header('Breakfast Menu')
@@ -15,6 +13,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # import the data
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Picklist so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
