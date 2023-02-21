7cbd8fb6f2762ff020bf08bff811ba7b55fdf90f
Tony Tauro <anthony.tauro@gmail.com>
2023-02-21 14:00:40 -0500

    Update app2.py
    Fruityvice - more changes

--- app2.py
+++ app2.py
@@ -23,7 +23,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the full table on the page
 streamlit.dataframe(fruits_to_show)
 
-# New section to displya fruityvice api response
+# New section to display fruityvice api response
 streamlit.header('Fruitvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes data to the screen
+
+# take the json and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output it to the screen as a table
+streamlit.dataframe(fruityvice_normalized)
